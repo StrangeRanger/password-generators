4fc96c3de14b1b7487b63fa8fb03e28c1b00f33d
Hunter T <30395560+StrangeRanger@users.noreply.github.com>
2017-11-18 18:52:44 -0800

    Updated the spacing and symbols

--- random_pass.py
+++ random_pass.py
@@ -1,16 +1,18 @@
 import random
 
-lengthOfPassword = input("How long do you want the password to be? ")
-ascii = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?[|]{}^~@_"
+length_of_pass = int(input("How long do you want the password to be? "))
+ascii = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&’()*+,-./:;<=>?[|]{}^~@_"
 password = ""
 
 
-def passwordGenerator(passLength):
+def password_generator(pass_length):
     global password
-    for i in range(passLength):
-        randomNumber = random.randrange(0, len(ascii))
-        randomCharacter = ascii[randomNumber]
-        password = password + randomCharacter
+    for i in range(pass_length):
+        random_num = random.randrange(0, len(ascii))
+        random_character = ascii[random_num]
+        password = password + random_character
     print(password)
-passwordGenerator(lengthOfPassword)
+
+
+password_generator(length_of_pass)
 del password
