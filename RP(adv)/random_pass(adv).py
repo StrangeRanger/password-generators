c134ccfee92530faac591d7c217916276eeb62e2
huntert <hunterjthompson@comcast.net>
2018-06-11 21:14:01 +0000

    added new comment

--- RP(adv)/random_pass(adv).py
+++ RP(adv)/random_pass(adv).py
@@ -2,6 +2,7 @@ import re
 import random
 import string
 
+# makes sure that user input is valid and that any ValueErrors are circumvented
 while True:
     try:
         length_of_pass = int(input("How long do you want the password to be? "))
